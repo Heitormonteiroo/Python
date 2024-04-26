cda8b1b7c29891596cdbd24515f8d85a8b461780
XxHeitorMPxX <hmonteiro2005@gmail.com>
2024-04-26 12:45:49 -0400

    Ex novos

--- Projetos/ExerciciosAlg/BeeCrowd/Ex1151.py
+++ Projetos/ExerciciosAlg/BeeCrowd/Ex1151.py
@@ -2,11 +2,17 @@ i=int(input())
 n=0
 n2=0
 n1=0
+tudo=""
 while i>n:
-    print(str(n1+n2))
+    soma=n1+n2
     aux=n1
     n1=n2
     n2=n2+aux
     if n==0:
         n1=1
+        tudo=str(soma)
+    else:
+        tudo=tudo+" "+str(soma)
     n=n+1
+print(tudo)
+
