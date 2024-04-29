02f8b0122e5e272864c597c7a74c327edf183742
XxHeitorMPxX <hmonteiro2005@gmail.com>
2024-04-29 14:45:23 -0400

    Ex novos

--- Projetos/MinhasLoucuras/Truco.py
+++ Projetos/MinhasLoucuras/Truco.py
@@ -6,6 +6,7 @@ meuponto=0
 pontoadversario=0
 rodadas=int(input("Digite quantos pontos para terminar a partida: "))
 verificador1,verificador2,verificador3=0,0,0
+
 lista = [1.1, 1.2, 1.3, 1.4,
          2.1, 2.2, 2.3, 2.4,
          3.1, 3.2, 3.3, 3.4,
@@ -16,6 +17,7 @@ lista = [1.1, 1.2, 1.3, 1.4,
          8.1, 8.2, 8.3, 8.4,
          9.1, 9.2, 9.3, 9.4,
          10.1, 10.2, 10.3, 10.4]
+
 baralho= [
     "4 de ouros", '4 de espadas', "4 de copas", "4 de paus",
     "5 de ouros", "5 de espadas", "5 de copas", "5 de paus",
@@ -27,6 +29,7 @@ baralho= [
     "A de ouro", "A de espadas", "A de copas", "A de paus",
     "2 de ouros", '2 de espadas', "2 de copas", "2 de paus",
     "3 de ouros", '3 de espadas', "3 de copas", "3 de paus"]
+
 while placar1<rodadas and placar2<rodadas:
     cartas = random.sample(lista, 6)
     carta1, carta2, carta3, cartaadv1, cartaadv2, cartaadv3 = cartas
@@ -39,7 +42,7 @@ while placar1<rodadas and placar2<rodadas:
         escolha = input("Escolha a sua carta 1, 2 ou 3: ")
         cartaadv=random.choice(cartasadv)
         i = lista.index(cartaadv)
-        if escolha =='1' and verificador1!=1:
+        if escolha =='1' and verificador1!=1 :
             if carta1>cartaadv:
                 meuponto = meuponto+1
                 print("Voce ganhou!!")
@@ -53,7 +56,10 @@ while placar1<rodadas and placar2<rodadas:
                 print("A do seu adversario foi {}".format(baralho[i]))
                 verificador1=1
             cartasadv.remove(cartaadv)
-            contador = contador - 1
+            if meuponto==2 or pontoadversario==2:
+                contador = 0
+            else:
+                contador = contador - 1
         elif escolha =='2' and verificador2!=1:
             if carta2>cartaadv:
                 meuponto = meuponto+1
@@ -61,6 +67,7 @@ while placar1<rodadas and placar2<rodadas:
                 print("Sua carta foi {}".format(baralho[indice2]))
                 print("A do seu adversario foi {}".format(baralho[i]))
                 verificador2 = 1
+
             else:
                 pontoadversario=pontoadversario+1
                 print("Voce perdeu :(")
@@ -68,7 +75,10 @@ while placar1<rodadas and placar2<rodadas:
                 print("A do seu adversario foi {}".format(baralho[i]))
                 verificador2 = 1
             cartasadv.remove(cartaadv)
-            contador = contador - 1
+            if meuponto==2 or pontoadversario ==2:
+                contador = 0
+            else:
+                contador = contador - 1
         elif escolha =='3' and verificador3!=1:
             if carta3>cartaadv:
                 meuponto = meuponto+1
@@ -84,7 +94,10 @@ while placar1<rodadas and placar2<rodadas:
                 print("A do seu adversario foi {}".format(baralho[i]))
                 verificador3 = 1
             cartasadv.remove(cartaadv)
-            contador = contador - 1
+            if meuponto==2 or pontoadversario==2:
+                contador=0
+            else:
+                contador = contador - 1
         else:
             print("Escolha corretamente")
     if meuponto>pontoadversario:
@@ -98,9 +111,13 @@ while placar1<rodadas and placar2<rodadas:
         pontoadversario = 0
         print("Voce perdeu a rodada:(")
     print("Placar:{} a {}!".format(placar1,placar2))
-
+    print("---------------")
+    print("PLACAR:{} a {}!".format(placar1, placar2))
+    print("PROXIMA RODADA")
+    print("---------------")
     verificador3,verificador2,verificador1,contador=0,0,0,3
 if placar1>placar2:
     print("Voce ganhou o jogo, o placar foi de {} a {}".format(placar1,placar2))
 else:
     print("Voce perdeu o jogo, o placar foi de {} a {}".format(placar2,placar1))
+#Resolver o negocio do jogo acabar errado colocar pra trucar
