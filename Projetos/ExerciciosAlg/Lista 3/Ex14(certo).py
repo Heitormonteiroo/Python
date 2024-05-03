11114a964b9f62864bc60fca378e9246820f5589
XxHeitorMPxX <167241925+XxHeitorMPxX@users.noreply.github.com>
2024-05-03 10:24:11 -0400

    Update Ex14(certo).py
    Corrigido

--- Projetos/ExerciciosAlg/Lista 3/Ex14(certo).py
+++ Projetos/ExerciciosAlg/Lista 3/Ex14(certo).py
@@ -2,7 +2,6 @@ n=int(input())
 condicao=("Nao existem numeros iguais consectutivos")
 while n>=1:
     ultimo_numero=int(n)%10
-    print(ultimo_numero)
     n=int(n)/10
     if ultimo_numero==(int(n)%10):
         condicao=("Existem numeros iguais consectutivos")
