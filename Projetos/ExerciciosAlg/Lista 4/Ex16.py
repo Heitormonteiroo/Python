ebfcf109fa0534b64a1b6cf9b0fcd817797e41f0
202419050263 <HMONTEIRO2005@GMAI.COM>
2024-06-19 08:07:38 -0400

    A

---
+++ Projetos/ExerciciosAlg/Lista 4/Ex16.py
@@ -0,0 +1,7 @@
+valores=input().split()
+i=0
+while i<len(valores):
+
+#usar uma lista para contadores e quando cada um aparecer coloca 1 no contador
+# e uma para ver os que ja apareceram como difereça
+# e uma outra lista para armazenar o numero que ja foi
