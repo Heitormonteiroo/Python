5e7d75512ff6d8eff2f35ef208f7915cf2bebce0
Heitor <hmonteiro2005@gmail.com>
2024-06-26 12:36:01 -0400

    sla

--- Projetos/ExerciciosAlg/miniprojeto/Matriculas.py
+++ Projetos/ExerciciosAlg/miniprojeto/Matriculas.py
@@ -1,4 +1,14 @@
 #caso tirar um aluno tirar ele de todas as diciplinas
+def colchetesint(a):
+    i=0
+    n=""
+    while i<(len(a)):
+        if i==0:
+            n = n + str(a[i])
+        else:
+            n=n+str(a[i])
+        i+=1
+    return n
 def escrever(nome,cpf):
     fp=open("Alunos.csv","a")
     fp.write("Nome: "+ str(nome) + " CPF: "+ str(cpf)+ "\n")
@@ -16,21 +26,33 @@ def verificar(cpf):
         i+=1
     fp.close()
     return condicao
+def achar(cpf):
+    fp=open("Alunos.csv","r")
+    linhas=fp.readlines()
+
+def lista():
+    fp=open("Alunos.csv","r")
+    linhas=fp.readlines()
+    fp.close()
+    return linhas
 
 
-print("----------------------------------------------------")
-print("Digite:")
-print("1-Gerenciamento ou cadastro de usuarios")
-print("2-Gerenciamento e cadastro diciplinas")
-print("3-Matricular aluno em uma diciplina")
-print("4-Cancelar a matricula de um aluno")
-print("5-Relatorio matriculas por diciplina")
-print("6-Sair")
-print("----------------------------------------------------")
+finalizar=True
+while finalizar:
 
-op1=int(input())
+    print("----------------------------------------------------")
+    print("Digite:")
+    print("1-Gerenciamento ou cadastro de usuarios")
+    print("2-Gerenciamento e cadastro diciplinas")
+    print("3-Matricular aluno em uma diciplina")
+    print("4-Cancelar a matricula de um aluno")
+    print("5-Relatorio matriculas por diciplina")
+    print("6-Sair")
+    print("----------------------------------------------------")
+    
+    op1=int(input())
 
-if op1==1:
+    if op1==1:
         continuar=True
         while continuar:
             print("-------------------------------------")
@@ -43,7 +65,9 @@ if op1==1:
             print("-------------------------------------")
             op2=int(input())
             if op2==1:
+                #Usar strip para deixar o cpf certo
                 certo1=True
+                
                 while certo1:
                     nome=input("Digite o nome: ")
                     cpf=input("Digite o CPF: ")
@@ -54,11 +78,21 @@ if op1==1:
                             escrever(nome,cpf)
                             certo=False
                     else:
-                        print("Digite o cpf corretamente:")
-                    certo1op=input("Digite 1 para digitar novamente ou qualquer tecla para voltar")
+                        print("Digite o cpf corretamente")
+                    print("Digite 1 para digitar novamente ou qualquer tecla para voltar")
+                    certo1op=input()
                     if certo1op!="1":
                         certo1=False
             if op2==2:
-                print("A")
+                certo1=True
+                while certo1:
+                    print("Digite 1 para abrir a lista de alunos ou digite o cpf")
+                    n=input()
+                    if n=="1":
+                        listainicial=lista()
+                        listafinal=colchetesint(listainicial)
+                        print(listafinal)
+                    else:
+                        
             if op2==5:
                 continuar=False
